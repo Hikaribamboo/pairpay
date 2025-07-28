c5de97cc0205ca18efbdc4365fcc5223b16decac
Hikaribamboo <hikari1653@gmail.com>
2025-07-28 14:30:20 +0900

    fixe durl

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -13,7 +13,9 @@ export default function Home() {
 
   useEffect(() => {
     if (!userId) {
-      const redirectUri = encodeURIComponent("https://pairpay/api/callback");
+      const redirectUri = encodeURIComponent(
+        "https://pairpay.vercel.app/api/callback"
+      );
       const loginUrl = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_LINE_CLIENT_ID}&redirect_uri=${redirectUri}&state=12345&scope=profile%20openid`;
 
       window.location.href = loginUrl;
