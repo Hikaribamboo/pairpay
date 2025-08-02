c6119ee25e3e142531edbc563ac56d6d68c3a81c
Hikaribamboo <hikari1653@gmail.com>
2025-08-02 16:40:00 +0900

    fix ui

--- src/app/payments/components/UnApprovedRequestList.tsx
+++ src/app/payments/components/UnApprovedRequestList.tsx
@@ -62,7 +62,7 @@ const UnApprovedRequestList = ({
 
   return (
     <div className="max-w-4xl mx-auto p-4">
-      <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">
+      <h1 className="text-2xl font-700 text-center text-gray-800 mb-6">
         未認証リクエスト一覧
       </h1>
 
