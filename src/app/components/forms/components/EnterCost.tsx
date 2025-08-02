16540f5d254c428f0860cecbaada1a12eac95965
Hikaribamboo <hikari1653@gmail.com>
2025-08-02 19:46:37 +0900

    fixed form error

--- src/app/components/forms/components/EnterCost.tsx
+++ src/app/components/forms/components/EnterCost.tsx
@@ -9,7 +9,7 @@ type Props = {
 const EnterCost = ({ paymentCost, setPaymentCost }: Props) => {
   const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   const handleCostButton = (digit: number) => {
-    setPaymentCost((prev) => prev + digit);
+    setPaymentCost((prev) => prev * 10 + digit);
   };
 
   const handleDelete = () => {
